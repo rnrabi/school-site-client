6692089a76ee84f520cd0cb5bf73a6b831a8ad89
rnrabi <rnrabi913@gmail.com>
2024-11-04 08:57:33 +0600

    first commit

---
+++ src/Router.jsx
@@ -0,0 +1,12 @@
+
+import {
+    createBrowserRouter
+} from "react-router-dom";
+import "./index.css";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div> HI ,Rabindro ,, Hello world!</div>,
+    },
+]);
