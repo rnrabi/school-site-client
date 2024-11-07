469bb4516efc3626506386803b43021bf95d9a26
rabindro <rnrabi915@gmail.com>
2024-11-07 14:48:37 +0600

    new branch rnrabi for creating pull request

--- src/Router.jsx
+++ src/Router.jsx
@@ -7,6 +7,6 @@ import "./index.css";
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div> HI ,Rabindro ,, Hello world!</div>,
+        element: <div> HI ,Rabindro , I am created a pull in rnrabi branch from main branch</div>,
     },
 ]);
