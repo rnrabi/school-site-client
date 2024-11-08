f72ac643a9b71144edac09a51b1a287cff502ae9
rnrabi <rnrabi913@gmail.com>
2024-11-08 21:51:30 +0600

    navber is ok

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { FaHome, FaLock } from "react-icons/fa";
+import { FcAbout } from 'react-icons/fc';
+import { GiTeacher } from 'react-icons/gi';
+import { MdConnectWithoutContact } from 'react-icons/md';
+import { PiStudentFill } from 'react-icons/pi';
 
 const Navbar = () => {
     return (
@@ -23,7 +28,7 @@ const Navbar = () => {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
+                            <li><a>Home</a></li>
                             <li>
                                 <a>Parent</a>
                                 <ul className="p-2">
@@ -36,10 +41,13 @@ const Navbar = () => {
                     </div>
                     <a className="btn btn-ghost text-xl">daisyUI</a>
                 </div>
+
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
+                        <li><a><FaHome />Home</a></li>
+                        <li><a><PiStudentFill />Classes</a></li>
+                        <li><a><GiTeacher />Teachers</a></li>
+                        {/* <li>
                             <details>
                                 <summary>Parent</summary>
                                 <ul className="p-2">
@@ -47,12 +55,34 @@ const Navbar = () => {
                                     <li><a>Submenu 2</a></li>
                                 </ul>
                             </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        </li> */}
+                        <li><a><FcAbout />About Us</a></li>
+                        <li><a><MdConnectWithoutContact />Contact Us</a></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="btn"><FaLock />Login</a>
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img
+                                    alt="Tailwind CSS Navbar component"
+                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                            </div>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
