5b0b1ad1b9ee5c9c395cc3047767d2b8c9281829
rnrabi <rnrabi913@gmail.com>
2024-11-08 22:32:56 +0600

    slider and about school section is set up

---
+++ src/component/AboutSchool.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutSchool = () => {
+    return (
+        <div>
+            This is about school ..... 
+        </div>
+    );
+};
+
+export default AboutSchool;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from '../component/Navbar';
+import Slider from '../component/Slider';
+import AboutSchool from '../component/AboutSchool';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Slider></Slider>
+            <AboutSchool></AboutSchool>
         </div>
     );
 };
